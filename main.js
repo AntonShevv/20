f8d5a09ef4b5151ec1cd7c71984a333b350a6bad
Anton <152779311+AntonShevv@users.noreply.github.com>
2026-03-20 11:57:24 +0300

    static

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ app.engine('hbs', engine({
         cancelButton: function() {
             return '<a href="/" class="btn btn-cancel">Отказаться</a>';
         },
+
+
         eq: function(a, b) {
             return a === b;
         }
@@ -28,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use("/users", userRouter);
 
